b5f1c51e4ce25e83fa147da65847cde6cdf2b1dc
Roger Pelissoni <rogerpelissoni@hotmail.com>
2026-06-23 10:25:56 -0300

    apply style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import { QueryProvider } from "@/providers/QueryProvider";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
+import "./globals.css";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
-
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
 export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
